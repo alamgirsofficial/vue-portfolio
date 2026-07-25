ac5cce465efd8937d803a84e281714d616dda67a
Md. Alamgir <alamgirbahadur.bd@gmail.com>
2026-07-25 23:53:13 +0600

    Hero Section Added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,49 @@
-<script setup>
-import Navbar from './components/Navbar.vue'
-
-</script>
-
 <template>
   <div class=" min-h-screen bg-linear-to-br from-gray-900 via-blue-900 to-gray-900">
 
     <Navbar/>
+    <Hero/>
     
   </div>
 </template>
+<script>
+  import AOS from 'aos';
+  import 'aos/dist/aos.css';
+  import Navbar from './components/Navbar.vue'
+  import Hero from './components/Hero.vue'
+
+  import {ref,nextTick, onMounted } from 'vue';
+
+  export default {
+    components: {
+      Navbar,
+      Hero
+    },
+    setup() {
+
+      const loading =ref(true);
+      onMounted(async() => {
+        await nextTick();
+
+        AOS.init({
+          duration:800,
+          once: false,
+          offset: 100,
+          easing: 'ease-in-out',
+        });
+        setTimeout(() => {
+          loading.value = false;
+        },800);
+      });
+      return{loading};
+    }
+  }
+</script>
+<style lang="">
+  
+</style>
+
+
+
+
+
---
+++ src/components/Hero.vue
@@ -0,0 +1,101 @@
+<template>
+    <section class="relative w-full overflow-hidden min-h-screen flex items-center">
+
+        <div class="w-full px-5 sm:px-8 md:px-12 lg:px-8 max-w-5xl  mx-auto relative z-10">
+            <div class="grid lg:grid-cols-2 gap-10 xl:gap-14 items-center">
+               <!-- Left Side Content -->
+                <div class="text-white lg:text-left"
+                data-aos="fade-right">
+                    <h1 class="text-white font-bold text-4xl md:text-5xl lg:text-6xl leading-tight">
+                        Hi, I'm 
+                        <span class="text-transparent bg-clip-text bg-linear-to-r from-blue-400 to-cyan-400">Alamgir</span>
+                    </h1>
+                        <p class="text-blue-200 font-medium text-lg md:text-xl mt-4"
+                        data-aos="fade-right"
+                        data-aos-delay="200"
+                        >I'm a Full Stack Web Developer</p>
+                <p class="text-gray-300 pt-6 text-center lg:text-left mx-auto max-w-xl text-lg leading-relaxed "
+                data-aos="fade-right"
+                data-aos-delay="300">
+                    Lorem ipsum dolor sit amet, consectetur
+                     adipisicing elit. Natus possimus nostrum 
+                     hic corporis deserunt ab odit tenetur
+                </p>
+
+            <div class="flex items-center gap-4 pt-8 flex-col sm:flex-row sm:w-max sm:mx-auto lg:mx-0"
+            data-aos="fade-up"
+            data-aos-delay="300">
+            <button class="px-8 md:px-10 py-4 rounded-xl relative group w-full sm:w-max flex justify-center overflow-hidden bg-linear-to-r from-blue-600 to-cyan-600 hover:from-blue-500 hover:to-cyan-500 transition-all duration-300"> 
+                <span class="relative flex items-center justify-center text-white font-semibold text-lg"> 
+                    Contact Me  
+                    <ArrowRightIcon class="ml-2 w-5 h-5 group-hover:translate-x-1 transition-transform">                
+                    </ArrowRightIcon>
+                    </span>                
+            </button>
+            <button class=" border-2 border-blue-400 px-8 md:px-10 py-4 rounded-xl relative group w-full sm:w-max flex justify-center hover:bg-blue-400/10 transition-all duration-300">
+                <div class="flex items-center justify-center relative">
+                    <ArrowDownIcon class="w-5 h-5 text-blue-400 group-hover:translate-y-1 transition-transform">
+                        </ArrowDownIcon>
+                        <span Class="pl-3 text-blue-400 font-semibold text-lg">
+                            Download CV </span>                            
+                </div>
+            </button>
+            </div>
+            <div class=" flex items-center justify-center lg:justify-start gap-8 pt-12"
+            data-aos="fade-up" data-aos-delay="400">
+
+            <div class="text-center"> 
+                <div class="text-2xl font-bold text-blue-400"> 50+</div>
+                <div class="text-blue-200 text-sm">Projects</div>
+            </div>
+            <div class="text-center"> 
+                <div class="text-2xl font-bold text-blue-400"> 5+</div>
+                <div class="text-blue-200 text-sm">Years</div>
+            </div>
+            <div class="text-center"> 
+                <div class="text-2xl font-bold text-blue-400"> 40+</div>
+                <div class="text-blue-200 text-sm">Clients</div>
+            </div>
+            </div>
+            </div>
+
+            <!-- Right Side Content  -->
+            <!-- Image  -->
+             <div class="lg:h-full flex justify-center lg:justify-end ">
+                <div class="relative w-full max-w-md lg:max-w-md">
+                    <div class="relative z-10 bg-linear-to-r from-blue-600 to-cyan-600 shadow-2xl rounded-[50%_40%_30%_60%_/60%_30%_70%_30%] p-4 backdrop-blur-sm border border-blue-500/30 -translate-y-6" 
+                    data-aos="zoom-in" data-aos-delay="500">
+
+                        <div class="rounded-xl overflow-hidden" >
+                            <img :src="heroImage" alt=" My Profile" 
+                            loading="lazy" 
+                            class="w-[500px] h-[500px] object-contain -translate-y-8">    
+                        </div>
+                    </div>
+                    <div class="absolute -top-2 -right-4 w-10 h-10 bg-blue-400 rounded-full blur-sm opacity-50 animate-pulse
+                    ">
+ 
+                    </div>
+
+                </div>
+
+             </div>
+
+            
+
+        </div>
+    </div>
+    </section>
+</template>
+<script setup>
+import { 
+    ArrowDownIcon, 
+    ArrowRightIcon 
+} from '@heroicons/vue/16/solid';
+import heroImage from '../assets/img.png'
+
+
+</script>
+<style>
+    
+</style>
